ffb52e8e20c35902c7c6232a80c92ec8b08076ec
faisal <fslvrse2004@gmail.com>
2025-06-24 12:42:42 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,55 +1,55 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 
 from models import db, Message
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 CORS(app)
 migrate = Migrate(app, db)
+
 db.init_app(app)
 
-@app.route('/messages', methods=['GET'])
-def index_messages():
-    messages = Message.query.order_by(Message.created_at).all()
-    return jsonify([msg.to_dict() for msg in messages]), 200
 
 @app.route('/messages', methods=['POST'])
-def post_message():
+def create_message():
     data = request.get_json()
-    message = Message(body=data.get("body"), username=data.get("username"))
-    db.session.add(message)
+    new_msg = Message(body=data.get('body'), username=data.get('username'))
+    db.session.add(new_msg)
     db.session.commit()
-    return jsonify(message.to_dict()), 201
+    return jsonify(new_msg.to_dict()),201
+
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages= Message.query.order_by(Message.created_at).all()
+    return jsonify([msg.to_dict() for msg in messages]),200
 
 @app.route('/messages/<int:id>', methods=['PATCH'])
-def patch_message(id):
-    message = db.session.get(Message, id)
-    if not message:
-        return jsonify({"error": "Message not found"}), 404
-
+def update_message(id):
+    msg = db.session.get(Message, id)
+    if not msg:
+        return jsonify({'error': 'Message not found'}), 404
     data = request.get_json()
     if 'body' in data:
-        message.body = data['body']
+        msg.body = data['body']
     if 'username' in data:
-        message.username = data['username']
-
+        msg.username = data['username']
     db.session.commit()
-    return jsonify(message.to_dict()), 200
+    return jsonify(msg.to_dict()), 200
 
 @app.route('/messages/<int:id>', methods=['DELETE'])
-def remove_message(id):
-    message = db.session.get(Message, id)
-    if not message:
-        return jsonify({"error": "Message not found"}), 404
-
-    db.session.delete(message)
+def delete_message(id):
+    msg=db.session.get(Message, id)
+    if not msg:
+        return jsonify({'error': 'Message not found'}),404
+    db.session.delete(msg)
     db.session.commit()
-    return jsonify({"message": "Deleted successfully"}), 200
+    return jsonify({'message':'Delete'}),200
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -4,7 +4,7 @@ from sqlalchemy_serializer import SerializerMixin
 from datetime import datetime
 
 metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s"
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
 db = SQLAlchemy(metadata=metadata)
@@ -15,14 +15,14 @@ class Message(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(500))
     username = db.Column(db.String(50))
-    created_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
-    updated_at = db.Column(db.DateTime(timezone=True), onupdate=db.func.now())
+    created_at = db.Column(db.DateTime(timezone = True), server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(timezone = True), onupdate=db.func.now())
 
     def to_dict(self):
         return {
-            "id": self.id,
-            "body": self.body,
-            "username": self.username,
-            "created_at": self.created_at.isoformat() if self.created_at else None,
-            "updated_at": self.updated_at.isoformat() if self.updated_at else None
+            'id': self.id,
+            'body': self.body,
+            'username': self.username,
+            'created_at': self.created_at.isoformat() if self.created_at else None,
+            'updated_at': self.updated_at.isoformat() if self.updated_at else None,
         }
